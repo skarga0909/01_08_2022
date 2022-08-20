13eb21e535d8e3b6cd172b16bb4dbf43d10a3aba
Svetlana <skarga0909@gmail.com>
2022-08-20 22:11:50 +0300

    first commit

---
+++ library/authors/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
+
+
+from .models import Author
+
+
+class AuthorModelSerializer(ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
